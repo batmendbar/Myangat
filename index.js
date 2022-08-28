e7ea3c70ac3abe5fed6225614ad3ac1666ebe067
Batmend Batsaikhan <batmendbar@Batmends-MacBook-Air.local>
2022-08-28 12:40:55 +0800

    Added MongoDB stuff

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 3000
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://batmendbar:DOPl3dIiDwipcPhF@sandbox.3asrq.mongodb.net/sample_airbnb?retryWrites=true&w=majority');
+
+const airbnbSchema = {
+  listing_url: String
+}
+
+const Airbnb = mongoose.model('Airbnb', airbnbSchema)
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => {
+    Airbnb.find({}, function(err, airbnbs) {
+      console.log(airbnbs)
+      res.render('pages/index', {
+          airbnb_list: airbnbs
+      })
+    })
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
