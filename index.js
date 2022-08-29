82c5f11cd185a97b28047c8f4df847af181e98cf
Batmend Batsaikhan <batmendbar@Batmends-MacBook-Air.local>
2022-08-30 02:22:35 +0800

    Added general table structure

--- index.js
+++ index.js
@@ -3,23 +3,39 @@ const path = require('path')
 const PORT = process.env.PORT || 3000
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://batmendbar:DOPl3dIiDwipcPhF@sandbox.3asrq.mongodb.net/sample_airbnb?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://batmendbar:DOPl3dIiDwipcPhF@sandbox.3asrq.mongodb.net/competition_results?retryWrites=true&w=majority');
 
-const airbnbSchema = {
-  listing_url: String
+const performanceSchema = {
+  rank: Number, 
+  name: String,
+  school: String,
+  grade: String,  
+  region: String,  
+  firstDayScores: [Number],
+  secondDayScores: [Number]
 }
 
-const Airbnb = mongoose.model('Airbnb', airbnbSchema)
+const competitionDivisionSchema = {
+  competitionName: String,
+  yearHeld: Number,
+  division: String,
+  firstDayProblemCount: Number,
+  secondDayProblemCount: Number,
+  performances: [performanceSchema]
+}
+
+
+const Class = mongoose.model('class', competitionDivisionSchema)
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => {
-    Airbnb.find({}, function(err, airbnbs) {
-      console.log(airbnbs)
+    Class.find({}, function(err, classes) {
+      console.log(classes)
       res.render('pages/index', {
-          airbnb_list: airbnbs
+          class_list: classes
       })
     })
   })
