83cd6c33258ec069d63bff1f2fdcab487c49f998
Batmend Batsaikhan <batmendbar@Batmends-MacBook-Air.local>
2022-08-30 15:31:57 +0800

    Populated one Competition Database

--- seed.js
+++ seed.js
@@ -1,25 +1,41 @@
 const mongoose = require('mongoose');
+const fs = require('fs');
+const readline = require("readline");
+const stream = fs.createReadStream("./Нийслэл_2018_Шалгасан_Боловсруулсан.csv");
+const rl = readline.createInterface({ input: stream });
+
+let competition = {
+    competitionName: "Нийслэлийн Математикийн Олимпиад",
+    yearHeld: 2018,
+    division: "5-р анги",
+    firstDayProblemCount: 4, 
+    secondDayProblemCount: 0,
+    performances: []
+}
+
+rl.on("line", (row) => {
+    let performance = row.split(",");
+    console.log(performance.slice(3, 7))
+    competition.performances.push({
+        rank: performance[0],
+        name: performance[1],
+        school: performance[2],
+        grade: "Unknown",
+        region: "Unknown",
+        firstDayScores: performance.slice(3, 7),
+        secondDayScores: []
+    })
+});
+
+rl.on("close", () => {
+    const user = Class.create(competition, function (err, small) {
+        if (err) return handleError(err);
+    });
+});
+
 
 mongoose.connect('mongodb+srv://batmendbar:DOPl3dIiDwipcPhF@sandbox.3asrq.mongodb.net/competition_results?retryWrites=true&w=majority');
 
-let competition = {
-    competitionName: "Улсын Математикийн Олимпиад",
-    yearHeld: 2021,
-    division: "Ахлах Ангилал",
-    firstDayProblemCount: 3, 
-    secondDayProblemCount: 3,
-    performances: [
-        {
-            rank: 1,
-            name: "Энэрэлт",
-            school: "Сант",
-            grade: 10,
-            region: "Сүхбаатар Дүүрэг",
-            firstDayScores: [7, 7, 0],
-            secondDayScores: [7, 5, 2]
-        }
-    ]
-}
 
 const performanceSchema = {
     rank: Number, 
@@ -42,7 +58,5 @@ const competitionDivisionSchema = {
 
 const Class = mongoose.model('class', competitionDivisionSchema)
 
-const user = await Class.create(competition, function (err, small) {
-    if (err) return handleError(err);
-});
+
 
