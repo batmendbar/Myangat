47e395ae1261393de9a333594c0675b7855ebde0
Batmend Batsaikhan <batmendbar@Batmends-MacBook-Air.local>
2022-09-03 21:35:48 +0800

    Cleaned up index.js from commented-out code

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-// import { parse } from 'node-html-parser';
 import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url';
 import mongoose from 'mongoose'
-// const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000
-import {performanceSchema} from "./models.js"
 import {divisionResultSchema} from "./models.js"
 
-
 mongoose.connect('mongodb+srv://batmendbar:DOPl3dIiDwipcPhF@sandbox.3asrq.mongodb.net/competition_results?retryWrites=true&w=majority');
-
-
-
-// const competitionSchema = {
-//   competitionName: String,
-//   instances: []
-// }
 
 const app = express()
 const __filename = fileURLToPath(import.meta.url);
@@ -30,19 +19,11 @@ app
 
 const DivisionResult = mongoose.model('divisionResult', divisionResultSchema)
 
-// app.get('/', (req, res) => {
-//   Competition.find({}, function(err, classes) {
-//     res.render('pages/competitions', {
-//         class_list: classes
-//     })
-//   })
-// })
-
 app.get('/find_division_results/:competitionName/:year/:division/', (req, res) => {
   DivisionResult.findOne({
-      // "competitionName": req.params.competitionName,
-      // "year": req.params.year,
-      // "division": req.params.division
+      "competitionName": req.params.competitionName,
+      "year": req.params.year,
+      "division": req.params.division
     }, 
     function(err, division_results) {
       res.render('pages/division_results', {
