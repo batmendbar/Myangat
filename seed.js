c3d1ad18f6f45346889d0f3dca75f0f170b9162f
Batmend Batsaikhan <batmendbar@Batmends-MacBook-Air.local>
2022-09-05 10:35:09 +0800

    Seed now automatically inserts into competition collection

--- seed.js
+++ seed.js
@@ -14,17 +14,8 @@ try {
     let headerPosition = {}
     
     let metadata = data.splice(0, 1)[0].split(',')
-    let division_result = {
-        competitionName: metadata[0],
-        year: metadata[1],
-        division: metadata[2],
-        dayOneProblemCount: dayOneProblemCount,
-        dayTwoProblemCount: dayTwoProblemCount,
-        performances: []
-    }
-
     let column_headers = data.splice(0, 1)[0].split(',')
-    // Competition.updateOne({competitionName: })
+
     for (let i = 0; i < column_headers.length; i++) {
         let header = column_headers[i];
         if (header == "Байр") {
@@ -57,6 +48,15 @@ try {
             headerPosition.award = i;
         }
     }
+
+    let division_result = {
+        competitionName: metadata[0],
+        year: metadata[1],
+        division: metadata[2],
+        dayOneProblemCount: dayOneProblemCount,
+        dayTwoProblemCount: dayTwoProblemCount,
+        performances: []
+    }
     
     data.forEach(performance => {
         performance = performance.split(',');
@@ -72,12 +72,54 @@ try {
             award: performance[headerPosition.award]
         });
     })
-    const divisionResult = DivisionResult.create(division_result);
-    
+
+    DivisionResult.deleteMany({
+        competitionName: metadata[0],
+        year: metadata[1],
+        division: metadata[2]
+    }).then(function(){
+        DivisionResult.create(division_result);
+        Competition.findOne(
+            {competitionName: metadata[0]}, function(err, competition) {
+                if (competition === null) {
+                    competition = {
+                        competitionName: metadata[0],
+                        instances: []
+                    }
+                }
+                
+                let targetYearIndex = -1;
+
+                for (let i = 0; i < competition.instances.length; i++) {
+                    if (competition.instances[i].year == metadata[1]) {
+                        targetYearIndex = i;
+                    }
+                }
+                if (targetYearIndex == -1) {
+                    competition.instances.push({year: metadata[1], divisions: []})       
+                    targetYearIndex = competition.instances.length - 1;
+                }
+
+                let targetDivisionIndex = -1;
+
+                for (let i = 0; i < competition.instances[targetYearIndex].divisions.length; i++) {
+                    if (competition.instances[targetYearIndex].divisions[i] == metadata[2]) {
+                        targetDivisionIndex = i;
+                    }
+                }
+
+                if (targetDivisionIndex == -1) {
+                    competition.instances[targetYearIndex].divisions.push(metadata[2])
+                }
+                Competition.create(competition)  
+                mongoose.disconnect();
+            }
+        )
+    })
+
 } catch (Err) {
   console.error(Err);
 }
-
 
 
 
